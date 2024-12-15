e1bf51a6a427277a3871f7b8418cda13fa5ae72b
meetgojiya98 <65299677+meetgojiya98@users.noreply.github.com>
2024-12-15 20:21:25 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
-import Projects from "../components/Projects/Projects";
+// import Projects from "../components/Projects/Projects";
 import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
